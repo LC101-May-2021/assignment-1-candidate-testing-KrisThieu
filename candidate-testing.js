4f9ca49635ebc97e807c45c135fd72bfe2c1a384
Kris Thieu <>
2021-06-03 23:53:22 +0000

    add a space after each questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,11 +10,11 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride",
@@ -32,9 +32,8 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  //candidateAnswer = input.question(question); 
   for (let i = 0; i < questions.length; i++) {
-    candidateAnswers.push(input.question(questions[i] + " "));
+    candidateAnswers.push(input.question(questions[i]));
   }
 }
 
@@ -42,11 +41,6 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  /*if (candidateAnswer === correctAnswer) {
-    console.log("correct");
-  } else {
-    console.log("incorrect");
-  }*/
   console.log();
   for (let i = 0; i < candidateAnswers.length; i++) {
     console.log(i + 1 + ") " + questions[i]);
@@ -68,7 +62,6 @@ function gradeQuiz(candidateAnswers) {
   let grade = (corrAns / questions.length) * 100;
 
   console.log(">>> Overall Grade: " + grade + "% (" + corrAns + " of 5 responses correct) <<<")
-
   console.log();
 
   if (grade >= 80)
@@ -77,7 +70,6 @@ function gradeQuiz(candidateAnswers) {
   } else {
     console.log(">>> Status: FAILED <<<")
   }
-
 
   return grade;
 }
